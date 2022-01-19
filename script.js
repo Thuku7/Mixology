29ff27ffce0ce3821ea2d1020bdf259dd640320c
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-01-19 12:40:33 +0300

    edits some bootstrap

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".cocktail1").mouseenter(function () {
+      $(".title1").show();
+    });
+    $(".cocktail1").mouseleave(function () {
+      $(".title1").hide();
+    });
+  });
+  $(document).ready(function () {
+    $(".portfolio2").mouseenter(function () {
+      $(".title2").show();
+    });
+    $(".portfolio2").mouseleave(function () {
+      $(".title2").hide();
+    });
+  });
+  $(document).ready(function () {
+    $(".portfolio3").mouseenter(function () {
+      $(".title3").show();
+    });
+    $(".portfolio3").mouseleave(function () {
+      $(".title3").hide();
+    });
+  });
+  $(document).ready(function () {
+    $(".portfolio4").mouseenter(function () {
+      $(".title4").show();
+    });
+    $(".portfolio4").mouseleave(function () {
+      $(".title4").hide();
+    });
+  });

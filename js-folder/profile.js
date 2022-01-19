052e7f47118b6b14ba5a65d6c1ace17e21747c69
Joseph-Wairimu <josephkamore084@gmail.com>
2022-01-19 17:03:08 +0300

    adding button upon hover

--- js-folder/profile.js
+++ js-folder/profile.js
@@ -16,14 +16,14 @@
           $(".imgg2").css("opacity", "1");
           $(".name" ,this).slideToggle(100, "linear").display(100, "linear");
           }
-        )});
+     )});
 
-        $(document).ready(function(){
-            $(".img3").hover(function(){
-              $(".imgg3").css("opacity", "0.5");
-              $(".name" ,this).slideToggle(100, "linear").display(100, "linear");
-              }, function(){
-              $(".imgg3").css("opacity", "1");
-              $(".name" ,this).slideToggle(100, "linear").display(100, "linear");
-              }
-            )});
+    $(document).ready(function(){
+        $(".img3").hover(function(){
+            $(".imgg3").css("opacity", "0.5");
+            $(".name" ,this).slideToggle(100, "linear").display(100, "linear");
+            }, function(){
+            $(".imgg3").css("opacity", "1");
+            $(".name" ,this).slideToggle(100, "linear").display(100, "linear");
+            }
+     )});

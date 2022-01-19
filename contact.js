96307a0edc4fd88ca6aa520aa1a12d96d13bcf5b
Memory-Wabwile <memorywabwile@gmail.com>
2022-01-19 17:17:13 +0300

    link the js file to html

---
+++ contact.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+  
+
+    
+// $("form").on("submit", function (e) {
+//     e.preventDefault();
+//     var name = $("#inputName").val();
+//     alert(
+//       "Hello" +
+//         " " +
+//         name +
+//         ",\nWe have received your message. " +
+//         "\nThank you for contacting us "
+//     );
+//     document.querySelector("form").reset();
+//   });
+// });
+// console.log("helllo")
